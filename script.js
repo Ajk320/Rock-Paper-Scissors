acc3865f3335247f2d5fbca0abe6750b46b38394
abhijay <abhijay320@gmail.com>
2023-12-27 11:35:03 +0530

    Created playRound() function

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 function getComputerChoice(){
     const myArray = ['rock', 'paper', 'scissors'];
     const randomElement = myArray[Math.floor(Math.random()*myArray.length)];
-    console.log(randomElement);
+    return randomElement;
 }
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection==computerSelection){
+        playRound(playerSelection, computerSelection);
+    }
+    else if(playerSelection=="rock" && computerSelection=="scissors"){
+        return "You Win! Rock beats Scissors"
+    }
+    else if(playerSelection=="rock" && computerSelection=="paper"){
+        return "You Lose! Paper beats Rock"
+    }
+    else if(playerSelection=="paper" && computerSelection=="rock"){
+        return "You Win! Paper beats Rock"
+    }
+    else if(playerSelection=="paper" && computerSelection=="scissors"){
+        return "You Lose! Scissors beat Paper"
+    }
+    else if(playerSelection=="scissors" && computerSelection=="rock"){
+        return "You Lose! Rock beats Scissors"
+    }
+    else if(playerSelection=="scissors" && computerSelection=="paper") {
+        return "You Win! Scissors beat Paper"
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
